b49f12b5f36ac48f45b6f4765db894f8a7bda2f0
frank.hein <frank.hein@maxence.de>
2020-09-23 20:41:07 +0200

    Work in progress. Working on dropship send error handling

--- Config/plugin.config.php
+++ Config/plugin.config.php
@@ -3,9 +3,19 @@
 namespace MxcVapee\Config;
 
 use MxcVapee\PluginListeners\StatusMailTemplateInstaller;
+use Shopware\Bundle\AttributeBundle\Service\TypeMapping;
 
 return [
     'plugin_listeners'   => [
         StatusMailTemplateInstaller::class,
+    ],
+    'doctrine' => [
+        'attributes' => [
+            's_articles_attributes'      => [
+                'mxcbc_product_type' => ['type' => TypeMapping::TYPE_STRING],
+                'mxcbc_product_meta' => ['type' => TypeMapping::TYPE_INTEGER],
+                'mxcbc_flavor'       => ['type' => TypeMapping::TYPE_STRING],
+            ],
+        ],
     ],
 ];
