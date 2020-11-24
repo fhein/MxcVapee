efa3178da6d32258b205787bca72889f0cdb8fc5
frank.hein <frank.hein@maxence.de>
2020-11-24 21:30:10 +0100

    Configuration changes.

--- Config/plugin.config.php
+++ Config/plugin.config.php
@@ -17,9 +17,10 @@ return [
     'doctrine' => [
         'attributes' => [
             's_articles_attributes'      => [
-                'mxcbc_product_type' => ['type' => TypeMapping::TYPE_STRING],
-                'mxcbc_product_meta' => ['type' => TypeMapping::TYPE_INTEGER],
-                'mxcbc_flavor'       => ['type' => TypeMapping::TYPE_STRING],
+                'mxcbc_product_type'    => ['type' => TypeMapping::TYPE_STRING],
+                'mxcbc_product_meta'    => ['type' => TypeMapping::TYPE_INTEGER],
+                'mxcbc_flavor'          => ['type' => TypeMapping::TYPE_STRING],
+                'mxcbc_flavor_filter'   => ['type' => TypeMapping::TYPE_STRING],
             ],
         ],
     ],
